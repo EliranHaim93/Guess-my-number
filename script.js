d8764e3bb19f7a539c6ad243a8b380beee7d4e51
EliranHaim93 <94759527+EliranHaim93@users.noreply.github.com>
2023-01-31 17:51:54 +0200

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(`.message`).textContent = message;
+};
+
+document.querySelector(`.check`).addEventListener(`click`, function () {
+  const guess = Number(document.querySelector(`.guess`).value);
+  console.log(typeof guess, guess);
+
+  // no input
+  if (!guess) {
+    displayMessage(`⛔ No number!`);
+
+    //win
+  } else if (guess === secretNumber) {
+    displayMessage(`Correct Number! 🎉`);
+    document.querySelector(`.number`).textContent = secretNumber;
+
+    document.querySelector(`body`).style.backgroundColor = `#60b347`;
+
+    document.querySelector(`.number`).style.width = `30rem`;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(`.highscore`).textContent = highScore;
+    }
+
+    // when guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? `Too high!📈` : `Too low!📉`);
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      displayMessage(`💥 You lost the game `);
+      document.querySelector(`.score`).textContent = 0;
+    }
+  }
+});
+
+document.querySelector(`.again`).addEventListener(`click`, function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  document.querySelector(`.number`).style.width = `15rem`;
+  displayMessage(`Start guessing... `);
+  document.querySelector(`.guess`).value = '';
+  document.querySelector(`.score`).textContent = score;
+  document.querySelector(`.number`).textContent = `?`;
+});
